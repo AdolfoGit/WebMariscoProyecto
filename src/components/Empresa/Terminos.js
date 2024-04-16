c1bae78f98e796a06a7c37a41b4fcced753ed47a
AdolfoGit <adolfoangelhernandezmanuel@gmail.com>
2024-04-15 18:15:43 -0600

    terminos no lo habia puesto jsjjs

--- src/components/Empresa/Terminos.js
+++ src/components/Empresa/Terminos.js
@@ -5,7 +5,7 @@ export default function Terminos() {
     <div className="aviso-privacidad">
     <h3 className='titulo' id='titulo'>Términos y Condiciones del Servicio</h3>
 
-    <h5>1. Pedidos en Físico:</h5>
+    <h5 className='font-bold'>1. Pedidos en Físico:</h5>
     <ul>
       <li>Los clientes pueden realizar pedidos de cualquier artículo del menú y barras, sujeto a restricciones de edad.</li>
       <li>Los pedidos grandes pueden ser acompañados por una cortesía antes de realizar el pedido.</li>
@@ -15,7 +15,7 @@ export default function Terminos() {
       <li>Es posible agregar más productos al pedido mientras se está cocinando o después de que se ha servido.</li>
     </ul>
 
-    <h5>2. Pedidos en Línea:</h5>
+    <h5 className='font-bold'>2. Pedidos en Línea:</h5>
     <ul>
       <li>La dirección de entrega debe ser verificada y estar ubicada dentro de Huejutla o sus alrededores para realizar un pedido.</li>
       <li>En caso de demoras superiores al tiempo estimado, se otorgará una cortesía al cliente en su próxima compra.</li>
@@ -23,14 +23,14 @@ export default function Terminos() {
       <li>No se aceptan cancelaciones para pedidos en línea.</li>
     </ul>
 
-    <h5>3. Métodos de Pago:</h5>
+    <h5 className='font-bold'>3. Métodos de Pago:</h5>
     <ul>
       <li>Se aceptan pagos con tarjeta de cualquier tipo y denominación.</li>
       <li>Se acepta efectivo, siempre y cuando esté en buenas condiciones.</li>
       <li>Todo cobro debe ser realizado por el gerente o dueño del establecimiento.</li>
     </ul>
 
-    <h5>4. Promociones:</h5>
+    <h5 className='font-bold'>4. Promociones:</h5>
     <ul>
       <li>Las promociones se ajustan a lo publicado en la página de Facebook.</li>
       <li>Las promociones se aplican en días festivos o en días específicos (10 de mayo, 14 de febrero, etc.).</li>
@@ -39,35 +39,43 @@ export default function Terminos() {
       <li>En los cumpleaños, el cumpleañero puede asistir sin pagar su cuenta si hay más de 10 personas en el local.</li>
     </ul>
 
-    <h5>5. Política de Reembolso:</h5>
+    <h5 className='font-bold'>5. Política de Reembolso:</h5>
     <ul>
       <li>Cualquier pedido incorrecto puede ser reemplazado sin costo adicional.</li>
       <li>Si un platillo no es del agrado del cliente debido a alergias o restricciones médicas, se permite un cambio.</li>
       <li>Las devoluciones tienen prioridad en el proceso de atención al cliente.</li>
     </ul>
 
-    <h5>6. Facturación:</h5>
+    <h5 className='font-bold'>6. Facturación:</h5>
     <ul>
       <li>Las facturas serán generadas y proporcionadas para cada pedido realizado.</li>
       <li>Cualquier problema relacionado con la facturación debe ser notificado y resuelto en un plazo de 2 días desde la fecha de emisión.</li>
     </ul>
 
-    <h5>7. Envíos:</h5>
+    <h5 className='font-bold'>7. Envíos:</h5>
     <ul>
       <li>Los envíos se realizarán dentro de la ubicación de Huejutla y sus alrededores.</li>
       <li>En caso de problemas con la entrega, se garantiza una solución adecuada, incluyendo reemplazo o descuento en pedidos futuros.</li>
     </ul>
 
-    <h5>8. Cómo Comprar:</h5>
+    <h5 className='font-bold'>8. Cómo Comprar:</h5>
     <ul>
       <li>Para realizar una compra, los clientes pueden seleccionar los productos deseados y seguir el proceso de pago indicado en la plataforma en línea o realizar el pedido en el establecimiento físico.</li>
       <li>Se proporcionarán instrucciones claras para completar el proceso de compra tanto en línea como en el establecimiento físico.</li>
     </ul>
 
-    <h5>9. Descuentos:</h5>
+    <h5 className='font-bold'>9. Descuentos:</h5>
     <ul>
       <li>Los descuentos serán aplicados según las promociones vigentes y las condiciones establecidas.</li>
       <li>Se proporcionarán descuentos adicionales a clientes frecuentes y en pedidos grandes como gesto de cortesía.</li>
+    </ul>
+
+    <h5 className='font-bold'>10. Reservaciones:</h5>
+    <ul>
+      <li>Las reservaciones deben realizarse con al menos un día de antelación respecto a la fecha deseada. El máximo de personas permitidas por reservación es de 4. Las reservaciones no son editables una vez confirmadas. Es responsabilidad del usuario proporcionar información precisa y actualizada al momento de realizar una reservación.</li>
+      <li>Las reservaciones realizadas estarán sujetas a una lista de espera, donde se determinara si se acepta la reservacion hecha por el usuario.</li>
+      <li>Para cancelar una reservación, se debe realizar con al menos un día de anticipación a la fecha establecida.</li>
+      <li>Si el usuario no llega a la reservación dentro de los primeros 20 minutos, la mesa reservada quedará disponible para otros clientes que se encuentren en el establecimiento.</li>
     </ul>
 
   </div>
