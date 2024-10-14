ee7524c20b02171b8361fb31b35255bf7719ee94
AdolfoGit <adolfoangelhernandezmanuel@gmail.com>
2024-10-13 21:41:05 -0600

    estrategia de cacheo

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,6 +5,8 @@ import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
+import { NetworkFirst } from 'workbox-strategies';
+
 
 clientsClaim();
 
@@ -14,23 +16,59 @@ precacheAndRoute(self.__WB_MANIFEST);
 // const desactivarPrecache = self.__WB_MANIFEST;
 // para más info: https://cra.link/PWA
 
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Instalación completada y archivos cacheados.');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activado y listo para servir desde la caché.');
+});
+
+
 const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
+
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== "navigate") {
+    // Si no es una solicitud de navegación, omitir.
+    if (request.mode !== 'navigate') {
       return false;
-    } // If this is a URL that starts with /\_, skip.
-    if (url.pathname.startsWith("/_")) {
-      return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+    }
+    
+    // Si la solicitud es un archivo (por ejemplo, una imagen), omitir.
     if (url.pathname.match(fileExtensionRegexp)) {
       return false;
-    } // Return true to signal that we want to use the handler.
+    }
+
+    // Devolver true para manejar la navegación con index.html
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+);
+
+// Cachear las respuestas de la API de productos
+registerRoute(
+  ({ url }) => {
+    if (url.origin === 'https://lacasadelmariscoweb.azurewebsites.net' && url.pathname.startsWith('/api/CasaDelMarisco/TraerProductos')) {
+      console.log('Interceptada solicitud de productos:', url.href);
+      return true; // Esto indica que la ruta es válida y debe ser manejada.
+    }
+    return false; // Esto indica que la solicitud no es de la API de productos.
+  },
+  new NetworkFirst({
+    cacheName: 'productos',
+    plugins: [
+      {
+        cacheWillUpdate: async ({ response }) => {
+          if (response && response.status === 200) {
+            console.log('Respuesta exitosa cacheada:', response);
+            return response;
+          }
+          console.log('Respuesta no cacheada (no exitosa):', response);
+          return null;
+        },
+      },
+    ],
+  })
 );
 
 registerRoute(
@@ -38,7 +76,7 @@ registerRoute(
   ({ url }) =>
     url.origin === self.location.origin && url.pathname.endsWith(".png"), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: "images",
+    cacheName: "image",
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
