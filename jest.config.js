3246e0c7f3c5945c0d0255c95ab62080d3dd7325
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-21 23:45:17 -0600

    skdfjklas

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
+// jest.config.js
 module.exports = {
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-    transformIgnorePatterns: ['node_modules/(?!gapi-script)'],
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Para manejar importaciones de estilos
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // Ajusta según tu proyecto
-  };
-  
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
