ce0377e4844fd7a510ec858e6a27c9f5756ef290
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-22 00:01:27 -0600

    kjlkjh

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript'
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime'
   ]
 };
