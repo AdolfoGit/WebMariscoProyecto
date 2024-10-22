80fccbea4944a2ea7b3b3f2be03c533e6c7b034c
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-22 15:22:55 -0600

    asdfa

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    supportFile: false, // Deshabilita el archivo de soporte si no es necesario
   },
 });
