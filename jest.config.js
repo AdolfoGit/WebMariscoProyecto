0c13fe0ba7d9fb2a2d5ec151dfbf262489a719b3
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-22 08:54:30 -0600

    klkñjkjñlkñ-lk

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
+    "^.+\\.scss$": "jest-scss-transform" // Asegúrate de tener este transformador instalado
+
   },
   transformIgnorePatterns: [
     'node_modules/(?!gapi-script)',
+    "/node_modules/(?!sweetalert2)"
+
   ],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
