818923ca77c20f36dc36617384dc87451f9158b3
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-21 23:47:43 -0600

    Gapi scripts

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
+  transformIgnorePatterns: [
+    'node_modules/(?!gapi-script)',
+  ],
 };
