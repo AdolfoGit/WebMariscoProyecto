208a649f6b5301f33d7ddaa50e4c80f050205a77
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-23 10:48:59 -0600

    listado de la URL

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,6 +1,6 @@
 describe('Sample Test', () => {
     it('visits the app', () => {
-      cy.visit('http://localhost:3000'); // Cambia a la URL de tu aplicación
+      cy.visit('https://web-marisco-proyecto-2z3t.vercel.app'); // Cambia a la URL de tu aplicación
       cy.contains('Welcome'); // Asegúrate de que esto sea un texto que exista en tu página de inicio
     });
   });
