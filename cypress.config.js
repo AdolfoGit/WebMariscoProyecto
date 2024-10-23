8dd8a3ad9044436157870f857454d1345a3373d0
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-23 10:23:12 -0600

    cambio a la ruta de baseURL

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     projectId: "zc1p9r",
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://web-marisco-proyecto-2z3t.vercel.app',
     supportFile: false,
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
   },
