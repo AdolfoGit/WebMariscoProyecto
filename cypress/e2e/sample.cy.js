3019feba4ccefcaae4649c5975c0a07857a710bf
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-10-23 10:56:08 -0600

    Cambios para poder hacer el test principal

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,7 +1,7 @@
 describe('Sample Test', () => {
     it('visits the app', () => {
       cy.visit('https://web-marisco-proyecto-2z3t.vercel.app'); // Cambia a la URL de tu aplicación
-      cy.contains('Welcome'); // Asegúrate de que esto sea un texto que exista en tu página de inicio
+      cy.contains('BROCHETA DE CAMARÓN'); // Asegúrate de que esto sea un texto que exista en tu página de inicio
     });
   });
   
