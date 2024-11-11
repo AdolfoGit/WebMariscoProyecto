383bd975cc30044f14141d99b8e8a9b69674cfb4
AdolfoGit <adolfoangelhernandezmanuel@gmail.com>
2024-11-11 16:22:14 -0600

    pwa cambios

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,6 +1,5 @@
-import React,{ createContext, useContext, useEffect, useState } from 'react';
-import PropTypes from 'prop-types'; // Importar PropTypes
-import Cookies from 'js-cookie';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import CryptoJS from 'crypto-js';
 
 const UserContext = createContext();
@@ -8,15 +7,14 @@ const ENCRYPTION_KEY = 'Soymainekko1#';
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    const storedUser = Cookies.get('userData');
+    const storedUser = localStorage.getItem('userData');
     if (storedUser) {
       const decryptedUser = CryptoJS.AES.decrypt(storedUser, ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8);
       setUser(JSON.parse(decryptedUser));
     }
-    const storedCart = Cookies.get('cart');
+    const storedCart = localStorage.getItem('cart');
     if (storedCart) {
       const decryptedCart = CryptoJS.AES.decrypt(storedCart, ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8);
       setCart(JSON.parse(decryptedCart));
@@ -25,35 +23,25 @@ export const UserProvider = ({ children }) => {
 
   const loginUser = (userData) => {
     setUser(userData);
-    Cookies.set('userData', CryptoJS.AES.encrypt(JSON.stringify(userData), ENCRYPTION_KEY).toString());
+    const encryptedUser = CryptoJS.AES.encrypt(JSON.stringify(userData), ENCRYPTION_KEY).toString();
+    localStorage.setItem('userData', encryptedUser);
   };
 
   const logoutUser = () => {
     setUser(null);
-    Cookies.remove('userData');
+    localStorage.removeItem('userData');
   };
 
-  const addToCart = (item) => {
-    const updatedCart = [...cart, item];
-    setCart(updatedCart);
-    Cookies.set('cart', CryptoJS.AES.encrypt(JSON.stringify(updatedCart), ENCRYPTION_KEY).toString());
-  };
-
-  const removeFromCart = (itemId) => {
-    const updatedCart = cart.filter(item => item.id !== itemId);
-    setCart(updatedCart);
-    Cookies.set('cart', CryptoJS.AES.encrypt(JSON.stringify(updatedCart), ENCRYPTION_KEY).toString());
-  };
 
   return (
-    <UserContext.Provider value={{ user, loginUser, logoutUser, cart, addToCart, removeFromCart }}>
+    <UserContext.Provider value={{ user,setUser, loginUser, logoutUser, }}>
       {children}
     </UserContext.Provider>
   );
 };
 
 UserProvider.propTypes = {
-  children: PropTypes.node.isRequired, // Validar que children es requerido y debe ser un nodo (cualquier cosa que React pueda renderizar)
+  children: PropTypes.node.isRequired,
 };
 
 export const useUser = () => {
--- src/components/Pedidos/Pedidos.js
+++ src/components/Pedidos/Pedidos.js
@@ -147,14 +147,14 @@ const Pedidos= () => {
         ) : pedidos.length > 0 ? (
           <div className="flex flex-wrap justify-start gap-6  mt-4 ">
             {pedidos.map((pedido) => (
-              <div key={pedido.IdPedido} className="bg-white rounded-lg shadow-lg p-10 w-full max-w-sm">
-                <div className="flex items-center justify-center mb-4">
+              <div key={pedido.IdPedido} className="bg-white rounded-lg shadow-lg p-10 w-full w-[23rem]">
+                <div className="flex items-center justify-center mb-2">
                   <div className="bg-white h-24 w-24 rounded-full p-2 mr-3">
                     <img src='https://scontent.fpbc2-1.fna.fbcdn.net/v/t39.30808-6/454348975_471022365692065_5380072197498364793_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFTW77Y7Oc_V1PP53rY6EfllKKjr-2y7MuUoqOv7bLsy6zs0jUcTLmS2x93OsGvz1IcvY1yzDjGv7M688081B3q&_nc_ohc=0MKdVX2lPygQ7kNvgGZpwmW&_nc_zt=23&_nc_ht=scontent.fpbc2-1.fna&_nc_gid=A_7bqFv1GfcNJqvONAosfNu&oh=00_AYAJ8fPZHCA_ISlmRsKGxO51U_WknzzYbGz3Q_HRxqCplQ&oe=671DF36E' alt="Logo" className="w-full h-full object-cover rounded-full"/>
                   </div>
                   <div>
-                    <h2 className="text-xl font-bold">La Casa Del Marisco</h2>
-                    <Typography className="text-xl text-gray-600">
+                    <h2 className="text-md font-bold">La Casa Del Marisco</h2>
+                    <Typography className="text-md text-gray-600">
                       {new Date(pedido.Fecha).toLocaleString()}
                     </Typography>
                   </div>
@@ -164,13 +164,13 @@ const Pedidos= () => {
                   <div className="space-y-8">
                     <div className="flex items-center relative">
                       <svg className="w-6 h-6 text-blue-500 ml-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                      <Typography variant='text' className="text-md text-end">
+                      <Typography variant='text' className="text-sm text-end">
                       20 de Diciembre Col. Comaltepec, Huejutla
                       </Typography>
                     </div>
                     <div className="flex items-center relative">
                       <svg className="w-6 h-6 text-gray-400 ml-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                      <Typography variant='text' className="text-md text-end">
+                      <Typography variant='text' className="text-sm text-end">
                       {pedido.Direccion.Calle} {pedido.Direccion.NumeroExterior}, Col. {pedido.Direccion.Colonia}, {pedido.Direccion.Ciudad}
                       </Typography>
                     </div>
@@ -180,8 +180,8 @@ const Pedidos= () => {
                 <div className="space-y-2 mb-4">
                   {pedido.Productos.map((producto) => (
                     <div key={producto.Id} className="flex justify-between">
-                      <Typography variant='text' className='text-md'>{producto.Nombre}</Typography>
-                      <Typography variant='text' className='text-md font-bold'>${producto.Precio.toFixed(2)}</Typography>
+                      <Typography variant='text' className='text-sm'>{producto.Nombre}</Typography>
+                      <Typography variant='text' className='text-sm font-bold'>${producto.Precio.toFixed(2)}</Typography>
                     </div>
                   ))}
                 </div>
