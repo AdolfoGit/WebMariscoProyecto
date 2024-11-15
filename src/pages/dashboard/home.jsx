eba420d13d2e1833d607ebdab726c48a65760a89
AdolfoGit <adolfoangelhernandezmanuel@gmail.com>
2024-11-15 01:35:06 -0600

    cambios

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import PedidosGeneral from "./pedidos";
 import DashboardGrafica from "./DashboardGraficas"
 import ReportesPago from "./BitacoraPedidos";
+import GraficaMovil from "./graficasMovil";
+
 export function Home() {
   return (
     <div className="mt-12">
       <div className="">
         <div className="justify-center items-center">
           <DashboardGrafica/>
+          <GraficaMovil/>
           <PedidosGeneral/>
-
-
         </div>
         <div className="col lg-6">
 
+          
           <ReportesPago/>
         </div>
       </div>
