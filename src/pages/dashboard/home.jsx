81ea4cce8a471e54e9747575a6806a8be1f39e7c
JafetEsauWerlybi <jeckooficial@gmail.com>
2024-11-15 05:09:54 -0600

    cambios de graficas movil

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import PedidosGeneral from "./pedidos";
 import DashboardGrafica from "./DashboardGraficas"
 import ReportesPago from "./BitacoraPedidos";
+import DashboardGrafica2 from "./DashboardGraficas2"
+
 export function Home() {
   return (
     <div className="mt-12">
       <div className="">
         <div className="justify-center items-center">
           <DashboardGrafica/>
+          <DashboardGrafica2/>
           <PedidosGeneral/>
 
 
